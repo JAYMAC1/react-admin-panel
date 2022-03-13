9e722abf1dd426cfd182e57b90037d4acd2ddfb4
James McNeil <mcneil_james@hotmail.com>
2022-03-13 19:25:23 +0000

    initial commit of Chart Features

---
+++ src/components/featuredChart/Feature.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './feature.scss'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
+const Feature = () => {
+  return (
+    <div className='featured'>
+      <div className='top'>
+        <h1 className='title'>Total Revenue</h1>
+        <MoreVertIcon fontSize='small' />
+      </div>
+      <div className='bottom'>
+        <div className='featuredChart'></div>
+      </div>
+    </div>
+  )
+}
+
+export default Feature
