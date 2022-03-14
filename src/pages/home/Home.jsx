9ef1648b7cfca2c3b7835b3fc2349522dd330c5e
James McNeil <mcneil_james@hotmail.com>
2022-03-14 11:08:46 +0000

    changed Table comp import to List

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Chart from '../../components/chart/Chart'
 import Feature from '../../components/featuredChart/Feature'
 import Navbar from '../../components/navbar/Navbar'
 import Sidebar from '../../components/sidebar/Sidebar'
+import List from '../../components/table/Table'
 import Widget from '../../components/widget/Widget'
 import './home.scss'
 
@@ -24,6 +25,7 @@ const Home = () => {
         </div>
         <div className='listContainer'>
           <div className='listTitle'>Latest Transactions</div>
+          <List />
         </div>
       </div>
     </div>
